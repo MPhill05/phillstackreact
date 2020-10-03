495e350cc2113a356e998dc1975e09ba65ccf8b8
Mark <mphill05@gmail.com>
2020-10-02 18:33:14 -0700

    adding header component and placeholders for future routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 
-import Header from './components/header/header.component';
+import HomePage from './pages/homepage/homepage.component';
+
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
       <Header />
-
+      {/* <Switch> */}
+        <Route exact path='/' component={HomePage} />
+      {/* </Switch> */}
     </div>
   );
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/phillstackLogo.svg';
+
+import './header.styles.scss';
 
 const Header = () => (
     <div className='header'>
-
+        <Link className='logo-container' to='/'>
+            <Logo className='logo' />
+        </Link>
+        <div className='options'>
+            <div className='option' to='/'>
+                About
+        </div>
+            <div className='option' to='/'>
+                Services
+        </div>
+            <div className='option' to='/'>
+                Portfolio
+        </div>
+            <div className='option' to='/'>
+                Contact
+        </div>
+        </div>
     </div>
 );
 
